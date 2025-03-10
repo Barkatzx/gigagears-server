321587f2b0904c22f70a32f9117de78571803148
Barkat Ullah <barkatullah.zx@gmail.com>
2025-03-11 03:47:20 +0600

    Role Based Route Added

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { login, logout, signup } from "../controllers/authController";
+import { isAdmin, isCustomer } from "../middleware/adminMiddleware";
 import { authMiddleware } from "../middleware/authMiddleware";
 
 const router = express.Router();
@@ -7,5 +8,16 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", authMiddleware, logout);
+
+//Admin customer dasboard
+// Admin-only route
+router.get("/admin-dashboard", authMiddleware, isAdmin, (req, res) => {
+  res.json({ message: "Welcome to the Admin Dashboard" });
+});
+
+// Customer-only route
+router.get("/customer-dashboard", authMiddleware, isCustomer, (req, res) => {
+  res.json({ message: "Welcome to the Customer Dashboard" });
+});
 
 export default router;
